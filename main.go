6c678e0a38eae9b138016572f9d66ff9781b5461
System Administrator <mppk2009@gmail.com>
2021-01-23 18:24:54 -0700

    Added first version of terminal quiz game

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
 	"os"
@@ -17,8 +18,47 @@ func main() {
 		exit(message)
 	}
 
-	_ = file
+	r := csv.NewReader(file)
 
+	lines, err := r.ReadAll()
+
+	problems := paresLines(lines)
+
+	correct := 0
+
+	for i, n := range problems {
+		fmt.Printf("QUESTION #%d: %s =\n", i+1, n.q)
+		var ans string
+		fmt.Scanln(&ans)
+
+		if ans == n.a {
+			correct++
+		}
+	}
+
+	fmt.Printf("%d out of %d is Correct \n", correct, len(problems))
+	if correct == len(problems) {
+		fmt.Println("All Correct, Great Job!")
+	} else {
+		fmt.Println("Try Again!")
+	}
+
+}
+
+type problem struct {
+	q string
+	a string
+}
+
+func paresLines(lines [][]string) []problem {
+	ret := make([]problem, len(lines))
+	for i, p := range lines {
+		ret[i] = problem{
+			q: p[0],
+			a: p[1],
+		}
+	}
+	return ret
 }
 
 func exit(errMessage string) {
